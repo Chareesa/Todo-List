b4df5b3c1c9d67ea08912fac36736f96b09d6f78
Stephen Wille <swille@dev9.com>
2018-03-15 20:59:07 -0700

    create categories called in rxjs.do

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Todo} from '../models/todo.model';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
@@ -13,300 +13,311 @@ const SHEET_URL = `${SmartsheetConfig.smartsheetUrl}/sheets/${SmartsheetConfig.s
 
 @Injectable()
 export class TodoService {
-  private todos: BehaviorSubject<Array<Todo>> = null;
+    private todos: BehaviorSubject<Array<Todo>> = null;
 
-  categories: Todo[] = [];
+    private categories: Todo[] = [];
 
-  constructor(private http: Http) {
-      this.todos = new BehaviorSubject(null);
-      this.http.get(`${SHEET_URL}`) /* -> result */
-      /* result -> */    .map(this.extractData) /* -> Json */
-      /* Json -> */      .map(this.fromJson) /* -> Array<Todo> */ 
-                         .catch((res: Response) => this.handleError(res))
-      /* Array<Todo> -> */    .subscribe(todos => this.todos.next(todos));
-  }
+    constructor(private http: Http) {
+        var self = this;
+        this.todos = new BehaviorSubject(null);
+        this.http.get(`${SHEET_URL}`) /* -> result */
+        /* result -> */.map(this.extractData) /* -> Json */
+        /* Json -> */.map(this.fromJson) /* -> Array<Todo> */
+            .do((data) => {
+               this.createCategories(data);
+            })
+            .catch((res: Response) => this.handleError(res))
+            .subscribe(todos => this.todos.next(todos));
+    }
 
-  displayIt() {
-    var temp = document.getElementsByTagName("template")[0];
-    var clon = temp.content.cloneNode(true);
-    document.body.appendChild(clon);
-  }
-  createCategories() {
-    
-    // let categories = this.todos;
-      // let lastParentNode;
-      // let categoryNum = 1;
-      // todos.forEach(function(todo) {
-      //   console.log('THE', todo);
-      //   this.categories.push(todo);
+    displayIt() {
+        var temp = document.getElementsByTagName("template")[0];
+        var clon = temp.content.cloneNode(true);
+        document.body.appendChild(clon);
+    }
+
+    private createCategories(data) {
+        console.log('..creating categories ', arguments);
+        data.forEach(todo => {
+           todo.woot = 'woot';
+        });
+
+        return data;
+        // let categories = this.todos;
+        // let lastParentNode;
+        // let categoryNum = 1;
+        // todos.forEach(function(todo) {
+        //   console.log('THE', todo);
+        //   this.categories.push(todo);
         // console.log('todo single', todo);
-          // if (!todo.parentId) {
-          //     if (lastParentNode) {
-                  
-          //         //all.push(lastParentNode);
-          //         if (!lastParentNode.isRowCategory) {
-          //             lastParentNode.category = 0;
-          //         }
-          //         lastParentNode = '';
-          //     }
-          //     if (todo.name) {
-          //         lastParentNode = todo;
-          //     }
-          // } else if (lastParentNode) {
+        // if (!todo.parentId) {
+        //     if (lastParentNode) {
 
-          //     if (todo.parentId === lastParentNode.id) {
-                
-          //         todo.category = lastParentNode.category;
-          //         if (!lastParentNode.isRowCategory) {
-          //             lastParentNode.isRowCategory = true;
-          //             lastParentNode.category = categoryNum;
-          //             categories.push(lastParentNode);
-          //             categoryNum++;
-          //         }
-          //         //lastParentNode = '';
-          //     } else {
-          //         //all.push(lastParentNode);
-          //         // lastParentNode = '';
-          //     }
-          // }
-      // })
-      // console.log('in cats', this.categories);
+        //         //all.push(lastParentNode);
+        //         if (!lastParentNode.isRowCategory) {
+        //             lastParentNode.category = 0;
+        //         }
+        //         lastParentNode = '';
+        //     }
+        //     if (todo.name) {
+        //         lastParentNode = todo;
+        //     }
+        // } else if (lastParentNode) {
 
-      //this.TodoService.categories = categories;
-  }
+        //     if (todo.parentId === lastParentNode.id) {
 
-  getTodos(): Observable<Array<Todo>> {
-      return this.todos;
-  }
+        //         todo.category = lastParentNode.category;
+        //         if (!lastParentNode.isRowCategory) {
+        //             lastParentNode.isRowCategory = true;
+        //             lastParentNode.category = categoryNum;
+        //             categories.push(lastParentNode);
+        //             categoryNum++;
+        //         }
+        //         //lastParentNode = '';
+        //     } else {
+        //         //all.push(lastParentNode);
+        //         // lastParentNode = '';
+        //     }
+        // }
+        // })
+        // console.log('in cats', this.categories);
 
-  deleteTodo(todoToDelete: Todo) {
-     this.deleteTodos([todoToDelete.rowId]);
-  }
+        //this.TodoService.categories = categories;
+    }
 
-  deleteTodos(todoIds: Array<number>) {
-      const todosToDelete = todoIds.join();
+    getTodos(): Observable<Array<Todo>> {
+        return this.todos;
+    }
 
-      this.http.delete(`${SHEET_URL}/rows?ids=${todosToDelete}`)
-          .map(this.extractData)
-          .catch((res: Response) => this.handleError(res))
-          .subscribe(deletedTodos => {
-              let remainingTodos;
-              this.todos.subscribe(todos => remainingTodos = todos.filter(todo => !deletedTodos.includes(todo.rowId)));
-              if (remainingTodos) {
-                  this.todos.next(remainingTodos);
-              }
-          });
-  }
+    deleteTodo(todoToDelete: Todo) {
+        this.deleteTodos([todoToDelete.rowId]);
+    }
 
-  addTodo(newTodoTitle: string) {
-      const newTodoJson = this.toNewTodoJson(newTodoTitle);
-      this.http.post(`${SHEET_URL}/rows`, JSON.stringify(newTodoJson))
-          .map(this.extractData)
-          .map(this.fromJson)
-          .catch((res: Response) => this.handleError(res))
-          .subscribe(newTodo => {
-              let todos;
-              this.todos.subscribe(todosSub => todos = todosSub);
-              if (todos) {
-                  todos.push(newTodo[0]);
-                  this.todos.next(todos);
-              }
-          });
-  }
+    deleteTodos(todoIds: Array<number>) {
+        const todosToDelete = todoIds.join();
 
-  updateTodo(todoToUpdate: Todo): Observable<Array<Todo>> {
-      this.http.put(`${SHEET_URL}/rows`, this.toUpdateTodoJson(todoToUpdate))
-          .map(this.extractData)
-          .map(this.fromJson)
-          .catch((res: Response) => this.handleError(res))
-          .subscribe(newTodo => {
-              let todos;
-              this.todos.subscribe(todosSub => {
-                  todos = todosSub.filter(todo => todo.rowId !== newTodo[0].rowId);
-              });
-              if (todos) {
-                  todos.push(newTodo[0]);
-                  this.todos.next(todos);
-              }
-          });
-      return this.todos;
-  }
+        this.http.delete(`${SHEET_URL}/rows?ids=${todosToDelete}`)
+            .map(this.extractData)
+            .catch((res: Response) => this.handleError(res))
+            .subscribe(deletedTodos => {
+                let remainingTodos;
+                this.todos.subscribe(
+                    todos => remainingTodos = todos.filter(todo => !deletedTodos.includes(todo.rowId)));
+                if (remainingTodos) {
+                    this.todos.next(remainingTodos);
+                }
+            });
+    }
 
-  private extractData(res: Response) {
-      const body = res.json();
-      return (body) ? body.result || body : {};
-  }
+    addTodo(newTodoTitle: string) {
+        const newTodoJson = this.toNewTodoJson(newTodoTitle);
+        this.http.post(`${SHEET_URL}/rows`, JSON.stringify(newTodoJson))
+            .map(this.extractData)
+            .map(this.fromJson)
+            .catch((res: Response) => this.handleError(res))
+            .subscribe(newTodo => {
+                let todos;
+                this.todos.subscribe(todosSub => todos = todosSub);
+                if (todos) {
+                    todos.push(newTodo[0]);
+                    this.todos.next(todos);
+                }
+            });
+    }
 
-  private fromJson(json: any): Array<Todo> {
-      console.log('JSON HERE', json);
-      const todos = new Array<Todo>();
-      let rows = null;
+    updateTodo(todoToUpdate: Todo): Observable<Array<Todo>> {
+        this.http.put(`${SHEET_URL}/rows`, this.toUpdateTodoJson(todoToUpdate))
+            .map(this.extractData)
+            .map(this.fromJson)
+            .catch((res: Response) => this.handleError(res))
+            .subscribe(newTodo => {
+                let todos;
+                this.todos.subscribe(todosSub => {
+                    todos = todosSub.filter(todo => todo.rowId !== newTodo[0].rowId);
+                });
+                if (todos) {
+                    todos.push(newTodo[0]);
+                    this.todos.next(todos);
+                }
+            });
+        return this.todos;
+    }
 
-      if (json.rows) {
-          rows = json.rows;
-      } else {
-          if (json instanceof Array) {
-              rows = json;
-          } else {
-              rows = [json];
-          }
-      }
+    private extractData(res: Response) {
+        const body = res.json();
+        return (body) ? body.result || body : {};
+    }
 
-      rows.forEach(row => {
-          const todo = new Todo(row.id, row.rowNumber, row.dueDate);
-          if (row.parentId) {
-            todo.parentId = row.parentId;
-          }
-          row.cells.forEach(cell => {
-              switch (cell.columnId) {
-                  case SmartsheetConfig.taskNameColumnId:
-                      todo.name = cell.value;
-                      break;
-                  case SmartsheetConfig.dueDateColumnId:
-                      todo.dueDate = cell.value;
-                      break;
-                  case SmartsheetConfig.doneColumnId:
-                      todo.done = cell.value ? cell.value : false;
-              }
-          });
+    private fromJson(json: any): Array<Todo> {
+        console.log('JSON HERE', json);
+        const todos = new Array<Todo>();
+        let rows = null;
 
-          if (todo.name) {
-            todos.push(todo);
-          }
-      });
-      return todos;
-  }
+        if (json.rows) {
+            rows = json.rows;
+        } else {
+            if (json instanceof Array) {
+                rows = json;
+            } else {
+                rows = [json];
+            }
+        }
 
-  private toNewTodoJson(newTodoTitle: string) {
-      return {
-          toBottom: true,
-          cells: [
-              {columnId:  SmartsheetConfig.taskNameColumnId, value: newTodoTitle}
-          ]
-      };
-  }
+        rows.forEach(row => {
+            const todo = new Todo(row.id, row.rowNumber, row.dueDate);
+            if (row.parentId) {
+                todo.parentId = row.parentId;
+            }
+            row.cells.forEach(cell => {
+                switch (cell.columnId) {
+                    case SmartsheetConfig.taskNameColumnId:
+                        todo.name = cell.value;
+                        break;
+                    case SmartsheetConfig.dueDateColumnId:
+                        todo.dueDate = cell.value;
+                        break;
+                    case SmartsheetConfig.doneColumnId:
+                        todo.done = cell.value ? cell.value : false;
+                }
+            });
 
-  private toUpdateTodoJson(todo: Todo) {
-      return {
-          id: todo.rowId,
-          cells: [
-              {
-                  columnId: SmartsheetConfig.taskNameColumnId,
-                  value: todo.name
-              },
-              {
-                  columnId: SmartsheetConfig.doneColumnId,
-                  value: todo.done
-              },
-              {
-                  columnId: SmartsheetConfig.dueDateColumnId,
-                  value: todo.dueDate
-              }
-          ]
-      };
-  }
+            if (todo.name) {
+                todos.push(todo);
+            }
+        });
+        return todos;
+    }
 
-  private handleError(error: Response | any) {
-      // In a real world app, you might use a remote logging infrastructure
-      let errMsg: string;
-      if (error instanceof Response) {
-          const body = error.json() || '';
-          const err = body.error || JSON.stringify(body);
-          errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
-      } else {
-          errMsg = error.message ? error.message : error.toString();
-      }
-      console.error(errMsg);
-      return Observable.throw(errMsg);
-  }
+    private toNewTodoJson(newTodoTitle: string) {
+        return {
+            toBottom: true,
+            cells: [
+                {columnId: SmartsheetConfig.taskNameColumnId, value: newTodoTitle}
+            ]
+        };
+    }
 
-  // Simulate POST /todos
-  // addTodo(todo: Todo): TodoService {
-  //   if (!todo.id) {
-  //     todo.id = ++this.lastId;
-  //   }
-  //   this.todos.push(todo);
-  //   return this;
-  // }
+    private toUpdateTodoJson(todo: Todo) {
+        return {
+            id: todo.rowId,
+            cells: [
+                {
+                    columnId: SmartsheetConfig.taskNameColumnId,
+                    value: todo.name
+                },
+                {
+                    columnId: SmartsheetConfig.doneColumnId,
+                    value: todo.done
+                },
+                {
+                    columnId: SmartsheetConfig.dueDateColumnId,
+                    value: todo.dueDate
+                }
+            ]
+        };
+    }
 
-  // Simulate DELETE /todos/:id
-  // deleteTodoById(id: number): TodoService {
-  //   this.todos = this.todos
-  //     .filter(todo => todo.id !== id);
-  //   return this;
-  // }
+    private handleError(error: Response | any) {
+        // In a real world app, you might use a remote logging infrastructure
+        let errMsg: string;
+        if (error instanceof Response) {
+            const body = error.json() || '';
+            const err = body.error || JSON.stringify(body);
+            errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+        } else {
+            errMsg = error.message ? error.message : error.toString();
+        }
+        console.error(errMsg);
+        return Observable.throw(errMsg);
+    }
 
-  // Simulate PUT /todos/:id
-  // updateTodoById(id: number, values: Object = {}): Todo {
-  //   const todo = this.getTodoById(id);
-  //   if (!todo) {
-  //     return null;
-  //   }
-  //   Object.assign(todo, values);
-  //   return todo;
-  // }
+    // Simulate POST /todos
+    // addTodo(todo: Todo): TodoService {
+    //   if (!todo.id) {
+    //     todo.id = ++this.lastId;
+    //   }
+    //   this.todos.push(todo);
+    //   return this;
+    // }
 
-  // Simulate GET /todos
-  // getAllTodos(): Todo[] {
-  //   return this.todos;
-  // }
+    // Simulate DELETE /todos/:id
+    // deleteTodoById(id: number): TodoService {
+    //   this.todos = this.todos
+    //     .filter(todo => todo.id !== id);
+    //   return this;
+    // }
 
-  // Simulate GET /todos/:id
-  // getTodoById(id: number): Todo {
-  //   return this.todos
-  //     .filter(todo => todo.id === id)
-  //     .pop();
-  // }
+    // Simulate PUT /todos/:id
+    // updateTodoById(id: number, values: Object = {}): Todo {
+    //   const todo = this.getTodoById(id);
+    //   if (!todo) {
+    //     return null;
+    //   }
+    //   Object.assign(todo, values);
+    //   return todo;
+    // }
 
-  // Simulate GET /todos/:category
-  // getTodoByCategory(id: number): Todo[] {
-  //   return this.todos
-  //     .filter(todo => todo.category === id);
-  // }
+    // Simulate GET /todos
+    // getAllTodos(): Todo[] {
+    //   return this.todos;
+    // }
 
-  // Toggle todo complete
-  // toggleTodoComplete(todo: Todo) {
-  //   const updatedTodo = this.updateTodoById(todo.id, {
-  //     complete: !todo.complete
-  //   });
-  //   return updatedTodo;
-  // }
+    // Simulate GET /todos/:id
+    // getTodoById(id: number): Todo {
+    //   return this.todos
+    //     .filter(todo => todo.id === id)
+    //     .pop();
+    // }
 
-  // Simulate POST /categories
-  // addCategory(category: Category): CategoryService {
-  //   if (!category.id) {
-  //     category.id = ++this.lastId;
-  //   }
-  //   this.categories.push(category);
-  //   return this;
-  // }
+    // Simulate GET /todos/:category
+    // getTodoByCategory(id: number): Todo[] {
+    //   return this.todos
+    //     .filter(todo => todo.category === id);
+    // }
 
-  // Simulate DELETE /categories/:id
-  // deleteCategoryById(id: number): CategoryService {
-  //   this.categories = this.categories
-  //     .filter(category => category.id !== id);
-  //   return this;
-  // }
+    // Toggle todo complete
+    // toggleTodoComplete(todo: Todo) {
+    //   const updatedTodo = this.updateTodoById(todo.id, {
+    //     complete: !todo.complete
+    //   });
+    //   return updatedTodo;
+    // }
 
-  // Simulate PUT /categories/:id
-  // updateCategoryById(id: number, values: Object = {}): Category {
-  //   let category = this.getCategoryById(id);
-  //   if (!category) {
-  //     return null;
-  //   }
-  //   Object.assign(category, values);
-  //   return category;
-  // }
+    // Simulate POST /categories
+    // addCategory(category: Category): CategoryService {
+    //   if (!category.id) {
+    //     category.id = ++this.lastId;
+    //   }
+    //   this.categories.push(category);
+    //   return this;
+    // }
 
-  // Simulate GET /categories
-  // getAllCategories(): Todo[] {
-  //   return this.categories;
-  // }
+    // Simulate DELETE /categories/:id
+    // deleteCategoryById(id: number): CategoryService {
+    //   this.categories = this.categories
+    //     .filter(category => category.id !== id);
+    //   return this;
+    // }
 
-  // // Simulate GET /categories/:id
-  // getCategoryById(id: number): Category {
-  //   return this.categories
-  //     .filter(category => category.id === id)
-  //     .pop();
-  // }
+    // Simulate PUT /categories/:id
+    // updateCategoryById(id: number, values: Object = {}): Category {
+    //   let category = this.getCategoryById(id);
+    //   if (!category) {
+    //     return null;
+    //   }
+    //   Object.assign(category, values);
+    //   return category;
+    // }
+
+    // Simulate GET /categories
+    // getAllCategories(): Todo[] {
+    //   return this.categories;
+    // }
+
+    // // Simulate GET /categories/:id
+    // getCategoryById(id: number): Category {
+    //   return this.categories
+    //     .filter(category => category.id === id)
+    //     .pop();
+    // }
 }
